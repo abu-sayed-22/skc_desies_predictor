306bc35951441cc3affdd1b072a7688f2b67655b
Sayed <mdsaied386@gmail.comm>
2023-09-20 13:02:17 +0600

    semi_final_01

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ ONly test purpose
 """
 
 
-
 import os
 from flask import Flask, request, render_template, redirect, url_for, make_response
 import base64
@@ -15,8 +14,12 @@ import json
 # loiading the learner
 from fastcore.all import *
 from fastai.learner import load_learner
-
+from flask_cors import CORS
 app = Flask(__name__)
+
+
+# setting up the cors for ignorring the cors policy
+CORS(app)
 
 
 @app.get('/')
@@ -37,36 +40,62 @@ def predict():
     }
 
     """
-    data = json.loads(request.data)
-    base64_image_data = data["image"]
-    # at first convert the base64 image into pil formate image
     try:
-        binary_data = base64.b64decode(base64_image_data)
-        image = Image.open(BytesIO(binary_data))
-        with open("hello.jpg", 'wb') as img:
-            img.write(binary_data)
-        # image.show()
-    except (base64.binascii.Error, OSError, Exception) as e:
-        return make_response({"error": "500", "message": "Conversion is not poossible"}, 500)
+        # loading the data from the user and get image base64 string
+        data = json.loads(request.data)
+        base64_image_data = data["image"]
+        apikey = data["apikey"]
 
-    # now fit to the model and return the result to the user
-    learner = load_learner("./export.pkl")
-    # result =learner.predict(image)
-    pred_class, pred_idx, outputs = learner.predict('hello.jpg')
-    os.unlink('./hello.jpg')
+        # checking the api key so that false request cannot be send
+        if apikey!="sayedSKC@386":
+            return make_response({"error": "401 ", "message": "Unauthorized "}, 401)
 
-    return {
-        "output": {
-            "pred_class": pred_class
+
+
+
+
+        # at first convert the base64 image into image
+        try:
+            binary_data = base64.b64decode(base64_image_data)
+            # image = Image.open(BytesIO(binary_data))
+            with open("input_img.jpg", 'wb') as img:
+                img.write(binary_data)
+            # image.show()
+        except (base64.binascii.Error, OSError, Exception) as e:
+
+            return make_response({"error": "500", "message": "Conversion is not poossible"}, 500)
+
+        # now fit to the model and return the result to the user
+        learner = load_learner("./export.pkl")
+        # result =learner.predict(image)
+        pred_class, pred_idx, proabs = learner.predict('input_img.jpg')
+        # deleting the image file for cleaning the gubage
+        os.unlink('./input_img.jpg')
+        proab_labels = ['potato_normal', 'potato_late_blight',
+                        'totamto_normal', 'totamto_late_blight']
+        proabs = proabs.numpy().tolist()
+        
+        return {
+            "output": {
+                "pred_class": pred_class,
+                "proab_labels": proab_labels,
+                "proabs": proabs
+
+            }
         }
-    }
+    except:
+        # handeling any else case
+        return  make_response({"status": 500, "message": "internal server error"}, 500)
 
 
-# @app.route()
-# def error_route():
-#     return{
-#         "error":"Generalize",
-#     }
+
+
+# Setting up the default 404 error handler
+@app.errorhandler(404)
+def page_not_found(error):
+    return make_response({"status": 404, "message": "page not found"}, 404)
+
+
 # finally run the app
 if __name__ == "__main__":
     app.run(debug=False)
